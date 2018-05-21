b7d5a399b01ab00d2e745c3d68f10fc1bcf451ee
J <jeremiaxhlin@gmail.com>
2018-05-21 13:52:37 -0700

    Add express route to handle user registration
    Task# 51

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const router = express.Router();
+const userController = require("../controllers/userController");
 
 router.get("/", (req, res) => {
   res.send("Hello From Chat App");
+});
+
+/**
+ * API
+ */
+router.post("/api/v1/register", (req, res) => {
+  res.send("register user!");
 });
 
 module.exports = router;
