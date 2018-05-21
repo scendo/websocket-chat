77af3400eca4b4648be7212e505746c1fe0cf349
J <jeremiaxhlin@gmail.com>
2018-05-21 12:42:41 -0700

    Add User model
    The login/Register features need a Mongodb User Model to store/access users in the application.
    
    In the future, additional fields will need to be added to User model to accommodate chat specific functionality
    
    Task# 50

---
+++ models/User.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+mongoose.Promise = global.Promise;
+const validator = require("validator");
+const mongodbErrorHandler = require("mongoose-mongodb-errors");
+const passportLocalMongoose = require("passport-local-mongoose");
+
+const userSchema = new Schema({
+  registered: {
+    type: Date,
+    default: Date.now
+  },
+  email: {
+    type: String,
+    unique: true,
+    lowercase: true,
+    trim: true,
+    validate: [validator.isEmail, "Invalid Email Address"],
+    required: "Please Supply an email address"
+  },
+  name: {
+    type: String,
+    required: "Please supply a name",
+    trim: true
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true
+  }
+});
+
+userSchema.plugin(passportLocalMongoose, { usernameField: "email" });
+userSchema.plugin(mongodbErrorHandler);
+
+userSchema.virtual("id").get(function() {
+  return this._id.toHexString();
+});
+
+module.exports = mongoose.model("User", userSchema);
