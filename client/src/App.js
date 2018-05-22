376e041563ee1dca6a552dbbe7e61b0cefe24671
J <jeremiaxhlin@gmail.com>
2018-05-22 12:50:13 -0700

    Add redux for client side state management
    Client side applications typically go through a series of interactions between the application and the user.
    
    ie:
     - login
     - update profile
     - search query
     - comment on a blog post
    
    Redux provides a means to track these changes within the application.
    The library's emphasis on immutability allows for the ability to "time travel" and step through changes.
    
    Redux Terminology:
    
     Store
    
     - Refers to the data set being used/tracked inside of the application
    
       ie:
         - users
         - chat messages
         - logged in/out
    
     Actions
    
     - Events that occur throughout the application that may alter the state of the application.
     - Basically, actions decide which reducers the data needs to pass through to update the application state.
    
     ie:
      - USER_LOGGED_IN
    
      - USER_LOGGED_OUT
    
     Reducers
    
     - The duty of a reducer is to update the data in the application state.
     - reducer's accept some data commonly referred as action.payload and returns the updated version of state.
    
    Redux Thunk middleware enables the ability return functions in redux actions vs plain object actions.
    Essentially, it allows for more granular controls on when a redux action is run.
    
    ie: You need to fetch some api data, and the action to be dispatched is dependent on the result of the api call.
    
    Task# 54

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,18 @@
 import React, { Component } from "react";
+import { Provider } from "react-redux";
+import store from "./store";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Websocket Chat Application</h1>
-        </header>
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          <header className="App-header">
+            <h1 className="App-title">Websocket Chat Application</h1>
+          </header>
+        </div>
+      </Provider>
     );
   }
 }
