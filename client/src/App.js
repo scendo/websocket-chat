520cd3f1c0903fabd38c0cd09ae34e9a85c9be60
J <jeremiaxhlin@gmail.com>
2018-05-22 15:06:09 -0700

    Add React Router and basic route
    React Router enables the application to better manage the browser location.
    
    Route component allows other components to be conditionally rendered based current url/path.
    
    Components nested inside of <Router /> and rendered using <Route /> are automatically passed a history prop
    which exposes some methods to work with routing. Most notably, history.push("/sample-url") may be used to update the current location/url in the browser.
    
    Task# 55

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,34 @@
 import React, { Component } from "react";
 import { Provider } from "react-redux";
 import store from "./store";
+import createHistory from "history/createBrowserHistory";
+import { Router } from "react-router";
+import { Route } from "react-router-dom";
 import "./App.css";
+
+const history = createHistory();
 
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <div className="App">
-          <header className="App-header">
-            <h1 className="App-title">Websocket Chat Application</h1>
-          </header>
-        </div>
+        <Router history={history}>
+          <Route
+            exact
+            path="/"
+            render={props => {
+              const { match, location, history } = props;
+
+              return (
+                <div className="App">
+                  <header className="App-header">
+                    <h1 className="App-title">Websocket Chat Application</h1>
+                  </header>
+                </div>
+              );
+            }}
+          />
+        </Router>
       </Provider>
     );
   }
