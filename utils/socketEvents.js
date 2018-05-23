771dc86bc14e2c1a20e3fab393580c7bf0dcd884
J <jeremiaxhlin@gmail.com>
2018-05-23 15:23:12 -0700

    Added socket event constants
    Socket events will grow over time. Having socket events listed in one place makes working with socket events more manageable.
    
    Task# 62

---
+++ utils/socketEvents.js
@@ -0,0 +1,44 @@
+//Default Socket io Events
+const ERROR = "error";
+const CONNECT = "connect";
+const DISCONNECT = "disconnect";
+const DISCONNECTING = "disconnecting";
+const NEW_LISTENER = "newListener";
+const REMOVE_LISTENER = "removeListener";
+const PING = "ping";
+const PONG = "pong";
+
+//Client Emits
+const CHAT_SERVICE_START = "CHAT_SERVICE_START";
+const ROOM_CREATE = "ROOM_CREATE";
+const ROOM_OPEN = "ROOM_OPEN";
+const ROOM_USER_LEAVING = "ROOM_USER_LEAVING";
+const MESSAGE_ADD = "MESSAGE_ADD";
+
+//Server Emits
+const USER_CONNECTED = "USER_CONNECTED";
+const USER_DISCONNECTED = "USER_DISCONNECTED";
+const ROOM_USER_JOINED = "ROOM_USER_JOINED";
+const ROOM_USER_LEFT = "ROOM_USER_LEFT";
+const MESSAGE_ADDED = "MESSAGE_ADDED";
+
+module.exports = {
+  ERROR,
+  CONNECT,
+  DISCONNECT,
+  DISCONNECTING,
+  NEW_LISTENER,
+  REMOVE_LISTENER,
+  PING,
+  PONG,
+  CHAT_SERVICE_START,
+  ROOM_CREATE,
+  ROOM_OPEN,
+  ROOM_USER_LEAVING,
+  MESSAGE_ADD,
+  USER_CONNECTED,
+  USER_DISCONNECTED,
+  ROOM_USER_JOINED,
+  ROOM_USER_LEFT,
+  MESSAGE_ADDED
+};
