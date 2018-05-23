4f9114c5b57f3056dbf6532dda606bed1dc5606f
J <jeremiaxhlin@gmail.com>
2018-05-23 16:56:55 -0700

    Add Message model
    Task# 64

---
+++ models/Message.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+mongoose.Promise = global.Promise;
+const mongodbErrorHandler = require("mongoose-mongodb-errors");
+
+const messageSchema = new Schema({
+  created: {
+    type: Date,
+    default: Date.now
+  },
+  user: {
+    type: String,
+    trim: true,
+    required: "Please supply a user for the message"
+  },
+  roomId: {
+    type: String,
+    trim: true,
+    required: "Please Supply a room id for the message"
+  },
+  value: {
+    type: String,
+    required: "Please supply a message value",
+    trim: true
+  }
+});
+
+// Duplicate the ID field.
+messageSchema.virtual("id").get(function() {
+  return this._id.toHexString();
+});
+
+// Ensure virtual fields are serialised.
+messageSchema.set("toJSON", {
+  virtuals: true
+});
+
+messageSchema.plugin(mongodbErrorHandler);
+
+module.exports = mongoose.model("Message", messageSchema);
