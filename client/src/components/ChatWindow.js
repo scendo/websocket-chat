f274c53ac4031242d0c857ce8cd301006b260856
J <jeremiaxhlin@gmail.com>
2018-05-23 13:40:28 -0700

    Add message submit functionality
    ChatWindow component holds the input field for message submissions.
    
    Controlled message input
     - the input field's value is set to the component's state.input
     - the state.input is updated on the the onChage
    
    Message Submit
    onKeyPress is used to to "submit" the message on "Enter" key press and subsequentially clearing state.input to empty the input field.
    At this point, the message input submission is visually what we expect, but the backend and integration with socket.io will be developed later.
    
    Task# 62

--- client/src/components/ChatWindow.js
+++ client/src/components/ChatWindow.js
@@ -3,6 +3,25 @@ import { Form } from "semantic-ui-react";
 class ChatWindow extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      input: ""
+    };
+    this.onInputChange = this.onInputChange.bind(this);
+    this.handleMessageSubmit = this.handleMessageSubmit.bind(this);
+  }
+
+  onInputChange(e) {
+    this.setState({ input: e.target.value });
+  }
+
+  handleMessageSubmit(e) {
+    if (this.state.input.length > 0 && e.key === "Enter") {
+      e.preventDefault();
+
+      this.setState({
+        input: ""
+      });
+    }
   }
 
   render() {
@@ -10,7 +29,15 @@ class ChatWindow extends Component {
       <div>
         <div id="chat-window" />
         <div>
-          <Form.Input id="m" name="message" autoComplete="off" fluid />
+          <Form.Input
+            id="m"
+            name="message"
+            autoComplete="off"
+            fluid
+            onChange={this.onInputChange}
+            onKeyPress={this.handleMessageSubmit}
+            value={this.state.input}
+          />
         </div>
       </div>
     );
