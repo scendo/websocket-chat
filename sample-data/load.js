f0d9d577862863d339e9ebc767a5ef8de029f079
J <jeremiaxhlin@gmail.com>
2018-05-23 17:07:16 -0700

    Add sample data for Message model
    Task# 64

--- sample-data/load.js
+++ sample-data/load.js
@@ -6,14 +6,24 @@ mongoose.connect(process.env.DATABASE);
 mongoose.Promise = global.Promise;
 
 // Import models
+const Message = require("../models/Message");
 const User = require("../models/User");
 
 // read and parse sample data json
+const messages = JSON.parse(
+  fs.readFileSync(__dirname + "/messages.json", "utf-8")
+);
 const users = JSON.parse(fs.readFileSync(__dirname + "/users.json", "utf-8"));
 
 const loadData = async () => {
   try {
-    await User.insertMany(users);
+    const insertMessagePromise = Message.insertMany(messages);
+    const insertUserPromise = User.insertMany(users);
+
+    await Promise.all([insertMessagePromise, insertUserPromise]).catch(e =>
+      console.log(e)
+    );
+
     console.log("loading sample data... complete!");
     process.exit();
   } catch (e) {
@@ -25,7 +35,14 @@ const loadData = async () => {
 
 const deleteData = async () => {
   console.log("Deleting data...");
-  await User.remove();
+
+  const removeMessagesPromise = Message.remove();
+  const removeUsersPromise = User.remove();
+
+  await Promise.all([removeMessagesPromise, removeUsersPromise]).catch(e =>
+    console.log(e)
+  );
+
   console.log("Data successfully deleted!");
   process.exit();
 };
