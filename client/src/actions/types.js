a18d83bee469aa1185257936efeea24478c6b1e5
J <jeremiaxhlin@gmail.com>
2018-05-31 14:32:49 -0700

    Add register redux action types
    REGISTER_SUCCESS
    REGISTER_ERROR
    
    Task# 75

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -2,6 +2,10 @@ export const USER_LOGGED_IN = "USER_LOGGED_IN";
 export const USER_LOGGED_OUT = "USER_LOGGED_OUT";
 export const SET_CURRENT_USER = "SET_CURRENT_USER";
 
+//Registration
+export const REGISTER_SUCCESS = "REGISTER_SUCCESS";
+export const REGISTER_ERROR = "REGISTER_ERROR";
+
 //Rooms
 export const ROOM_OPEN = "ROOM_OPEN";
 export const ROOM_ADD = "ROOM_ADD";
