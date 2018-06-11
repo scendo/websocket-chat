a44adb2d63d6020ba4ddecc0ce22b91c7ce5ce87
J <jeremiaxhlin@gmail.com>
2018-06-11 12:49:22 -0700

    Add sample-data helpers to modularize the logic
    Task# 94

---
+++ sample-data/helpers.js
@@ -0,0 +1,140 @@
+// Import models
+const Message = require("../models/Message");
+const Room = require("../models/Room");
+const User = require("../models/User");
+const UserMeta = require("../models/UserMeta");
+
+const mongoose = require("mongoose");
+mongoose.connect(process.env.DATABASE);
+mongoose.Promise = global.Promise;
+
+/**
+ * Load sample application data
+ *
+ * @param {} param0
+ */
+const loadData = async ({
+  users,
+  usermetas,
+  rooms,
+  channelsRooms,
+  directRooms,
+  messages
+}) => {
+  try {
+    const usermetas = getUserMetas({ users, directRooms });
+    const insertMessagePromise = Message.insertMany(messages);
+    const insertRoomPromise = Room.insertMany(rooms);
+    const insertUserPromise = User.insertMany(users);
+    const insertUserMetaPromise = UserMeta.insertMany(usermetas);
+
+    await Promise.all([
+      insertMessagePromise,
+      insertRoomPromise,
+      insertUserPromise,
+      insertUserMetaPromise
+    ]).catch(e => console.log(e));
+
+    console.log("loading sample data... complete!");
+    process.exit();
+  } catch (e) {
+    console.log("Error! loading sample data failed!");
+    console.log(e);
+    process.exit();
+  }
+};
+
+/**
+ * Delete sample application data
+ */
+const deleteData = async () => {
+  console.log("Deleting data...");
+
+  const removeMessagesPromise = Message.remove();
+  const removeRoomsPromise = Room.remove();
+  const removeUsersPromise = User.remove();
+  const removeUserMetaPromise = UserMeta.remove();
+
+  await Promise.all([
+    removeMessagesPromise,
+    removeRoomsPromise,
+    removeUsersPromise,
+    removeUserMetaPromise
+  ]).catch(e => console.log(e));
+
+  console.log("Data successfully deleted!");
+  process.exit();
+};
+
+/**
+ * Get a random color hex value
+ */
+const getRandomColor = () => {
+  const colors = [
+    "#001f3f",
+    "#85144b",
+    "#FF851B",
+    "#3D9970",
+    "#FFDC00",
+    "#B10DC9",
+    "#39CCCC",
+    "#2ECC40",
+    "#ea281e",
+    "#8e8e8e"
+  ];
+  return colors[Math.floor(Math.random() * colors.length)];
+};
+
+/**
+ * Get dynamically generated user metas
+ *
+ * roomMetas - metaField for each direct message a user is in
+ * badgeColor - color of the user's badge/circle next to their name in a message
+ * totalUnreadMessages - count of total unread direct messages
+ */
+const getUserMetas = ({ users, directRooms }) => {
+  const userMetas = [];
+
+  /**
+   * Dynamically generate a room meta document for each direct message a user is in room
+   */
+  const roomMetas = directRooms.reduce((arr, room) => {
+    const roomMetaPerUser = room.users.reduce((arr, userId) => {
+      return [
+        ...arr,
+        {
+          userId: userId,
+          key: `room_${room._id}`,
+          value: {
+            unreadMessageCount: 0
+          }
+        }
+      ];
+    }, []);
+
+    return [...arr, ...roomMetaPerUser];
+  }, []);
+
+  /**
+   * General userMeta for each user
+   */
+  const generalUserMeta = users.reduce((arr, user) => {
+    return [
+      ...arr,
+      {
+        userId: user._id,
+        key: "badgeColor",
+        value: getRandomColor()
+      }
+    ];
+  }, []);
+
+  return userMetas.concat(roomMetas, generalUserMeta);
+};
+
+module.exports = {
+  loadData,
+  deleteData,
+  getRandomColor,
+  getUserMetas
+};
--- sample-data/load.js
+++ sample-data/load.js
@@ -1,15 +1,12 @@
 require("dotenv").config({ path: __dirname + "/../variables.env" });
 const fs = require("fs");
 
-const mongoose = require("mongoose");
-mongoose.connect(process.env.DATABASE);
-mongoose.Promise = global.Promise;
-
-// Import models
-const Message = require("../models/Message");
-const Room = require("../models/Room");
-const User = require("../models/User");
-const UserMeta = require("../models/UserMeta");
+const {
+  loadData,
+  deleteData,
+  getRandomColor,
+  getUserMetas
+} = require("./helpers");
 
 // read and parse sample data json
 const messages = JSON.parse(
@@ -23,101 +20,17 @@ const directRooms = JSON.parse(
 );
 const rooms = channelsRooms.concat(directRooms);
 const users = JSON.parse(fs.readFileSync(__dirname + "/users.json", "utf-8"));
-
-const getRandomColor = () => {
-  const colors = [
-    "#001f3f",
-    "#85144b",
-    "#FF851B",
-    "#3D9970",
-    "#FFDC00",
-    "#B10DC9",
-    "#39CCCC",
-    "#2ECC40",
-    "#ea281e",
-    "#8e8e8e"
-  ];
-  return colors[Math.floor(Math.random() * colors.length)];
-};
-/**
- * Dynamically generate UerMeta for each direct message room
- */
-const usermetas = directRooms.reduce((arr, room) => {
-  const roomMetaPerUser = room.users.reduce((arr, userId) => {
-    return [
-      ...arr,
-      {
-        userId: userId,
-        key: `room_${room._id}`,
-        value: {
-          unreadMessageCount: 0
-        }
-      }
-    ];
-  }, []);
-
-  return [...arr, ...roomMetaPerUser];
-}, []);
-
-const badgeColorUserMetas = users.reduce((arr, user) => {
-  return [
-    ...arr,
-    {
-      userId: user._id,
-      key: "badgeColor",
-      value: getRandomColor()
-    }
-  ];
-}, []);
-
-const loadData = async () => {
-  try {
-    const insertMessagePromise = Message.insertMany(messages);
-    const insertRoomPromise = Room.insertMany(rooms);
-    const insertUserPromise = User.insertMany(users);
-    const insertUserMetaPromise = UserMeta.insertMany(usermetas);
-    const insertUserMetaBadgeColorPromise = UserMeta.insertMany(
-      badgeColorUserMetas
-    );
-
-    await Promise.all([
-      insertMessagePromise,
-      insertRoomPromise,
-      insertUserPromise,
-      insertUserMetaPromise,
-      insertUserMetaBadgeColorPromise
-    ]).catch(e => console.log(e));
-
-    console.log("loading sample data... complete!");
-    process.exit();
-  } catch (e) {
-    console.log("Error! loading sample data failed!");
-    console.log(e);
-    process.exit();
-  }
-};
-
-const deleteData = async () => {
-  console.log("Deleting data...");
-
-  const removeMessagesPromise = Message.remove();
-  const removeRoomsPromise = Room.remove();
-  const removeUsersPromise = User.remove();
-  const removeUserMetaPromise = UserMeta.remove();
-
-  await Promise.all([
-    removeMessagesPromise,
-    removeRoomsPromise,
-    removeUsersPromise,
-    removeUserMetaPromise
-  ]).catch(e => console.log(e));
-
-  console.log("Data successfully deleted!");
-  process.exit();
-};
+const usermetas = getUserMetas({ users, directRooms });
 
 if (process.argv.includes("--delete")) {
   deleteData();
 } else {
-  loadData();
+  loadData({
+    users,
+    usermetas,
+    rooms,
+    channelsRooms,
+    directRooms,
+    messages
+  });
 }
