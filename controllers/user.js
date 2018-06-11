08f1789273a14aefaea26ecd0b4d4763a2c9dae3
J <jeremiaxhlin@gmail.com>
2018-06-11 13:59:16 -0700

    Add default room and UserMeta on user register
    Task# 94

--- controllers/user.js
+++ controllers/user.js
@@ -1,9 +1,11 @@
 const mongoose = require("mongoose");
 const User = mongoose.model("User");
+const UserMeta = mongoose.model("UserMeta");
+const Room = mongoose.model("Room");
 const promisify = require("es6-promisify");
 const bcrypt = require("bcrypt");
 const { check, body, validationResult } = require("express-validator/check");
-
+const { getRandomColor } = require("../utils/chat");
 /**
  * Validate registration fields sent through the request
  *
@@ -72,13 +74,27 @@ exports.register = async (req, res) => {
   const salt = await bcrypt.genSalt(10);
   const hash = await bcrypt.hash(req.body.password, salt);
 
-  const newUser = await User.create({
-    name: req.body.name,
-    email: req.body.email,
-    password: hash
+  const defaultRoom = await Room.findOne({
+    name: "Community",
+    group: "channel"
   });
 
-  if (newUser) {
+  const newUser = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: hash,
+    rooms: [defaultRoom]
+  });
+
+  //Default userMeta
+  const userMetaPromise = UserMeta.create([
+    { userId: newUser._id, key: "badgeColor", value: getRandomColor() },
+    { userId: newUser._id, key: "totalUnreadMessages", value: 0 }
+  ]);
+
+  const result = await Promise.all([newUser.save(), userMetaPromise]);
+
+  if (result) {
     res.json({ success: true });
   }
   res.status(500);
