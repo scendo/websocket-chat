616a7bab9681c204ac97552dd2285184acf95fac
J <jeremiaxhlin@gmail.com>
2018-06-14 18:12:59 -0700

    Add rendering of totalUnreadMessages in mobile menu
    Task# 94

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,47 +1,92 @@
 import React, { Component } from "react";
-import { Icon, Menu, Dropdown } from "semantic-ui-react";
+import { Icon, Menu, Dropdown, Label } from "semantic-ui-react";
 
-const Navbar = props => {
-  const { currentUser, activeRoom } = props;
+class Navbar extends Component {
+  constructor(props) {
+    super(props);
+  }
 
-  const trigger = (
-    <span>
-      <Icon
-        color={currentUser.socketId ? "green" : null}
-        name={currentUser.socketId ? "circle" : "circle outline"}
+  /**
+   * Renders unread message label on mobile menu
+   */
+  renderTotalUnreadMessagesLabel() {
+    const { currentUser } = this.props;
+
+    //Prevent render error when component attempts to load before the chat data has finished fetching
+    if (Object.keys(currentUser).length === 0) {
+      return null;
+    }
+
+    const { totalUnreadMessages } = currentUser.metaData;
+
+    return (
+      <Label
+        className="unread-messages-label"
+        circular={true}
+        color="red"
+        floating={true}
+        style={{
+          position: "absolute",
+          top: "0.25em",
+          left: "4.7em",
+          display: totalUnreadMessages > 0 ? "initial" : "none"
+        }}
+        content={(() => {
+          //Cap the output of totalUnread messages to a single digitl
+          if (totalUnreadMessages > 0) {
+            if (totalUnreadMessages >= 10) {
+              return "9+";
+            }
+            return totalUnreadMessages;
+          }
+          return null;
+        })()}
       />
-      {currentUser.name}
-    </span>
-  );
+    );
+  }
+  render() {
+    const { currentUser, activeRoom } = this.props;
 
-  return (
-    <Menu id="chat-menu">
-      <Menu.Item name="sidebar-menu" onClick={props.handleMenuClick}>
-        <Icon name="sidebar" size="large" />
-      </Menu.Item>
-      <div id="room-header">
-        <span id="room-name">#{activeRoom.name}</span>
-      </div>
-      <Menu.Menu position="right">
-        <Dropdown
-          id="user-settings-menu"
-          trigger={trigger}
-          icon="dropdown"
-          item
-        >
-          <Dropdown.Menu position="right">
-            <Dropdown.Item
-              text="Logout"
-              onClick={props.handleLogoutClick}
-              style={{
-                textAlign: "center"
-              }}
-            />
-          </Dropdown.Menu>
-        </Dropdown>
-      </Menu.Menu>
-    </Menu>
-  );
-};
+    const trigger = (
+      <span>
+        <Icon
+          color={currentUser.socketId ? "green" : null}
+          name={currentUser.socketId ? "circle" : "circle outline"}
+        />
+        {currentUser.name}
+      </span>
+    );
+
+    return (
+      <Menu id="chat-menu">
+        <Menu.Item name="sidebar-menu" onClick={this.props.handleMenuClick}>
+          <Icon name="sidebar" size="large" />
+          {this.renderTotalUnreadMessagesLabel()}
+        </Menu.Item>
+        <div id="room-header">
+          <span id="room-name">#{activeRoom.name}</span>
+        </div>
+        <Menu.Menu position="right">
+          <Dropdown
+            id="user-settings-menu"
+            trigger={trigger}
+            icon="dropdown"
+            item
+          >
+            <Dropdown.Menu position="right">
+              <Dropdown.Item
+                text="Logout"
+                onClick={this.props.handleLogoutClick}
+                style={{
+                  textAlign: "center"
+                }}
+              />
+            </Dropdown.Menu>
+          </Dropdown>
+        </Menu.Menu>
+      </Menu>
+    );
+  }
+}
 
 export default Navbar;
